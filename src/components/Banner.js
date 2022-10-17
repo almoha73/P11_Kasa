bba72bd1f673cbfb0912985fc22e1f2a63627ebf
Agnes <almoha@gmail.com>
2022-10-18 01:11:25 +0200

    banner about height problem résolu

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,14 +1,12 @@
 import React from "react";
+import MediaQuery from "react-responsive";
 import about from "../assets/img/about.jpg";
 import home from "../assets/img/home.jpg";
 const Banner = ({ text, noText }) => {
 	return (
 		<>
 			{text && (
-				<div
-					className="dark-title"
-					style={{ height: "111px" }}
-				>
+				<div className="dark-title">
 					<div className="title">
 						<img src={home} alt="mountain" />
 						<h1>Chez vous, partout et ailleurs</h1>
@@ -16,15 +14,11 @@ const Banner = ({ text, noText }) => {
 				</div>
 			)}
 			{noText && (
-				<div
-				className="dark-title"
-				style={{ height: "223px" }}
-			>
-				<div className="title">
-					<img src={about} alt="mountain" />
-					
+				<div className="dark-title-about">
+					<div className="title">
+						<img src={about} alt="mountain" />
+					</div>
 				</div>
-			</div>
 			)}
 		</>
 	);
