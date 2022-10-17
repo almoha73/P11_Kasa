8d54b1f95e040f90063876fe2a36d373b965580f
Agnes <almoha@gmail.com>
2022-10-17 23:54:36 +0200

    fignolage

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,20 +1,32 @@
 import React from "react";
 import about from "../assets/img/about.jpg";
 import home from "../assets/img/home.jpg";
-const Banner = ({text}) => {
+const Banner = ({ text, noText }) => {
 	return (
-		<div className="dark-title">
-			<div className="title">
-				<img src={about} alt="" />
-
-				{text && (
-					<>
-						<img src={home} alt="" />
+		<>
+			{text && (
+				<div
+					className="dark-title"
+					style={{ height: "111px" }}
+				>
+					<div className="title">
+						<img src={home} alt="mountain" />
 						<h1>Chez vous, partout et ailleurs</h1>
-					</>
-				)}
+					</div>
+				</div>
+			)}
+			{noText && (
+				<div
+				className="dark-title"
+				style={{ height: "223px" }}
+			>
+				<div className="title">
+					<img src={about} alt="mountain" />
+					
+				</div>
 			</div>
-		</div>
+			)}
+		</>
 	);
 };
 
--- src/pages/About.js
+++ src/pages/About.js
@@ -9,7 +9,7 @@ const About = ({ dataAbout }) => {
   return (
     <div className="page-about">
     <Navigation />
-    <Banner />
+    <Banner noText/>
     <main className="main-about">
       <div className="accordion-about">
         {dataAbout && (
