25aeff7af8ad124a130185c9a187be0a03815c36
Agnes <almoha@gmail.com>
2022-10-19 20:08:19 +0200

    détails sss

--- src/pages/Error.js
+++ src/pages/Error.js
@@ -11,6 +11,7 @@ const Error = () => {
         <p>Oups! La page que vous demandez n'existe pas.</p>
         <Link to="/">Retourner sur la page d’accueil</Link>
       </div>
+      
     </main>
   );
 };
